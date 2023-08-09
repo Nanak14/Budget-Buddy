33e1738d846d7d750e0833cc2c56eb12e8abe970
Nanakpreet Singh <snanak1403@gmail.com>
2023-08-09 14:01:48 -0700

    Login UI chagne

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:budget_buddy/button.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,74 +8,101 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          toolbarHeight: 70,
-          title: const Center(
-            child: Text(
-              "Budget Buddy",
-              style: TextStyle(
-                fontFamily: 'Montserrat',
-                fontWeight: FontWeight.bold,
-                fontSize: 33,
-              ),
-            ),
-          ),
-          backgroundColor: const Color(0xFF272829),
-        ),
+        backgroundColor: Colors.grey[300],
         body: SafeArea(
+        child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
               Center(
-                child: Container(
-                  margin: const EdgeInsets.symmetric(vertical: 80),
-                  child: const Text(
-                    "LOGIN",
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontFamily: 'Montserrat',
+                child: Column(
+                  children: [
+                    Image.asset('images/background.png', height: 200, width: 200),
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 30),
+                      child: const Text(
+                        "LOGIN",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
+                ),
+              ),
+              
+              Text(
+                'Welcome to the ultimate budgeting app!',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
                 ),
               ),
               const SizedBox(height: 30,),
-              const Text(
-                "Email",
-                style: TextStyle(
-                  fontFamily: 'Montserrat',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-              Container(
-                color: const Color(0xFFD8D9DA),
-                margin: const EdgeInsets.all(20),
-                padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 160),
-                child: const Text("YO"),
-              ),
-              const Text(
-                "Pass",
-                style: TextStyle(
-                  fontFamily: 'Montserrat',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-              Container(
-                color: const Color(0xFFD8D9DA),
-                margin: const EdgeInsets.all(20),
-                padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 160),
-                child: const Text("YO"),
-              ),
 
+              // Textfield for Email.
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: TextField(
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey.shade400),
+                      ),
+                      fillColor: Colors.grey.shade200,
+                      filled: true,
+                      hintText: 'Username',
+                      hintStyle: TextStyle(color: Colors.grey[500])),
+                ),
+              ),
+              const SizedBox(height: 10,),
+
+              // TextField for Password.
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: TextField(
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey.shade400),
+                      ),
+                      fillColor: Colors.grey.shade200,
+                      filled: true,
+                      hintText: 'Password',
+                      hintStyle: TextStyle(color: Colors.grey[500])),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 25,),
+
+              MyButton(onTap: () {  },),
             ],
           ),
         ),
 
+      ),
       ),
     );
   }
